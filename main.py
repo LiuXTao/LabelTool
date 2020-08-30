48aa94f01a4aaa3a48ad063e92c27736b4cefeb9
LiuXTao <1416101719@qq.com>
2020-08-30 18:22:46 +0800

    first version

---
+++ main.py
@@ -0,0 +1,212 @@
+'''
+@File     : main.py
+@Copyright: 
+@author   : lxt
+@Date     : 2020/8/29
+@Desc     :
+'''
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
+from uiwindow import Ui_MainWindow
+import sys
+import os
+import pandas as pd
+import numpy as np
+
+
+class MainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.setupUi(self)
+        self.setWindowIcon(QIcon('./icon/icon.ico'))
+        self.setWindowTitle("标注小民工")
+        self.data = None
+        self.data_index = None
+        self.totalindex = 0
+        self.fileName = ""
+        self.emotion_value = self.emotion_list[0]
+        self.sarcasm = self.radio_list[0]
+        self.metaphor = self.radio_list[0]
+        self.exaggeration = self.radio_list[0]
+        self.homophonic = self.radio_list[0]
+        self.symbolism = self.radio_list[0]
+        self.sentiment = self.radio_list[0]
+        self.save_curr_flag = False
+        # add action
+        self.actionfileopen.triggered.connect(self._openFile)
+        self.cb_6.currentIndexChanged[str].connect(self._selectEmotion)
+        self.save_btn.clicked.connect(self._saveData)
+        self.next_btn.clicked.connect(self._clickNext)
+        self.bg1.buttonClicked.connect(self.rbclicked)
+        self.bg2.buttonClicked.connect(self.rbclicked)
+        self.bg3.buttonClicked.connect(self.rbclicked)
+        self.bg4.buttonClicked.connect(self.rbclicked)
+        self.bg5.buttonClicked.connect(self.rbclicked)
+        self.bg7.buttonClicked.connect(self.rbclicked)
+
+    def _openFile(self):
+        fileName, fileType = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", os.getcwd(),
+                                                                   "All Files(*);;Text Files(*.csv)")
+        self._reset()
+        self.fileName = fileName
+        self._loadFile(fileName)
+        self._showData()
+
+    def _reset(self):
+        self.data = None
+        self.data_index = None
+        self.totalindex = 0
+        self.fileName = ""
+        self.emotion_value = self.emotion_list[0]
+        self.save_curr_flag = False
+        self.emotion_value = self.emotion_list[0]
+        self.sarcasm = self.radio_list[0]
+        self.metaphor = self.radio_list[0]
+        self.exaggeration = self.radio_list[0]
+        self.homophonic = self.radio_list[0]
+        self.symbolism = self.radio_list[0]
+        self.sentiment = self.radio_list[0]
+        self._cleanText()
+
+    def _selectEmotion(self, i):
+        self.emotion_value = i
+
+    def _loadFile(self, file_path):
+        self.data = pd.read_csv(file_path, encoding='utf-8')
+        self.data_index = self.data.index
+        print(len(self.data_index))
+        print(self.data.shape)
+
+
+    def _showData(self):
+        nums = self.data.shape[0]
+        for i in range(self.totalindex, nums):
+            df = self.data.iloc[i]
+            f1, f2, f3, f4, f5, f6, f7, f8 = df['sarcasm'], df['metaphor'], df['exaggeration'], df['homophonic'], df['symbolism'], df['emotion'], df['sentiment'], df['other_subtext']
+            if pd.isnull(f1) or pd.isnull(f2) or pd.isnull(f3) or pd.isnull(f4) or pd.isnull(f5) or pd.isnull(f7) or pd.isnull(f8):
+                if 'question' not in self.data.columns:
+                    self.comment_text.setText(str(df['comment']))
+                    self.content_text.setText("{}".format(df['content']))
+                else:
+                    self.comment_text.setText(str(df['comment']))
+                    self.content_text.setText("{}\n{}".format(df['question'], df['content']))
+                self.totalindex = i
+                self._setLabel()
+                break
+            else:
+                continue
+
+    def _showPrev(self):
+        df = self.data.iloc[self.totalindex]
+        if 'question' not in self.data.columns:
+            self.comment_text.setText(str(df['comment']))
+            self.content_text.setText("{}".format(df['content']))
+        else:
+            self.comment_text.setText(str(df['comment']))
+            self.content_text.setText("{}\n{}".format(df['question'], df['content']))
+        self._setLabel()
+
+    def _clickNext(self):
+        flag = self._saveData()
+        if flag == True:
+            self.totalindex += 1
+            self._showData()
+            self.save_curr_flag = False
+            self._cleanText()
+            if self.totalindex % 10 == 0:
+                self._saveToFile()
+
+    def _show_message(self, message):
+        QtWidgets.QMessageBox.information(self, "信息提示框", message, QtWidgets.QMessageBox.Yes)
+
+    def _setLabel(self):
+        self.curr_label.setText("当前条:{}".format(self.totalindex))
+
+    def _saveData(self):
+        if self.data is None:
+            self._show_message(message="请选择并打开文件")
+            return False
+        if self.save_curr_flag == True:
+            return True
+        l1, sent1 = self.sarcasm, self.inputtext_1.text()
+        f1, data1 = self._constructor(l1, sents=sent1, flag=2)
+        print(data1)
+        l2, sent21, sent22 = self.metaphor, self.inputtext_21.text(), self.inputtext_22.text()
+        f2, data2 = self._constructor(l2, sents=sent21, sents2=sent22, flag=3)
+        l3, sent3 = self.exaggeration, self.inputtext_3.text()
+        f3, data3 = self._constructor(l3, sents=sent3, flag=2)
+
+        l4, sent41, sent42 = self.homophonic, self.inputtext_41.text(), self.inputtext_42.text()
+        f4, data4 = self._constructor(l4, sents=sent41, sents2=sent42, flag=3)
+
+        l5, sent51, sent52 = self.symbolism, self.inputtext_51.text(), self.inputtext_52.text()
+        f5, data5 = self._constructor(l5, sents=sent51, sents2=sent52, flag=3)
+        data6 = self.emotion_value
+        f7, data7 = self._constructor(self.sentiment, flag=1)
+        data8 = self.input_8.text() if self.input_8.text() != "" else "-"
+
+        if f1 and f2 and f3 and f4 and f5 and f7:
+            self.data.loc[self.data_index[self.totalindex],'sarcasm'] = data1
+            self.data.loc[self.data_index[self.totalindex],'metaphor'] = data2
+            self.data.loc[self.data_index[self.totalindex], 'exaggeration'] = data3
+            self.data.loc[self.data_index[self.totalindex], 'homophonic'] = data4
+            self.data.loc[self.data_index[self.totalindex], 'symbolism'] = data5
+            self.data.loc[self.data_index[self.totalindex], 'emotion'] = data6
+            self.data.loc[self.data_index[self.totalindex], 'sentiment'] = data7
+            self.data.loc[self.data_index[self.totalindex], 'other_subtext'] = data8
+            self.save_curr_flag = True
+            return True
+        self._show_message(message="输入不符合格式")
+        return False
+
+    def rbclicked(self):
+        sender = self.sender()
+        if sender == self.bg1:
+            self.sarcasm = self.radio_list[self.bg1.checkedId()]
+        elif sender == self.bg2:
+            self.metaphor = self.radio_list[self.bg2.checkedId()]
+        elif sender == self.bg3:
+            self.exaggeration = self.radio_list[self.bg3.checkedId()]
+        elif sender == self.bg4:
+            self.homophonic = self.radio_list[self.bg4.checkedId()]
+        elif sender == self.bg5:
+            self.symbolism = self.radio_list[self.bg5.checkedId()]
+        elif sender == self.bg7:
+            self.sentiment = self.radio_list[self.bg7.checkedId()]
+
+
+    def _saveToFile(self):
+        self.data.to_csv(self.fileName, index=False)
+
+    def _constructor(self, label, sents=None, flag=2, sents2=None):
+        if label not in ["-1", "0", "1"]:
+            return False, None
+        if flag == 2:
+            sents = "-" if sents == "" else sents
+            return True, "{};{}".format(label, sents)
+        elif flag ==3:
+            sents = "-" if sents == "" else sents
+            sents2 = "-" if sents2 == "" else sents2
+            return True, "{};{};{}".format(label, sents, sents2)
+        else:
+            return True, "{}".format(label)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, '信息', '确认退出吗？',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+            print("save")
+            self._saveToFile()
+        else:
+            event.ignore()
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    win = MainForm()
+    win.show()
+    sys.exit(app.exec_())
---
+++ uiwindow.py
@@ -0,0 +1,293 @@
+'''
+@File     : fileOpen.py
+@Copyright: 
+@author   : lxt
+@Date     : 2020/8/29
+@Desc     :
+'''
+
+import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget,QTextEdit,QVBoxLayout,QPushButton, QRadioButton,QButtonGroup, QLabel, QHBoxLayout, QLineEdit, QComboBox
+from PyQt5.QtGui import QIcon
+
+class Ui_MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super(Ui_MainWindow, self).__init__(parent)
+        self.setWindowTitle('QLineEdit例子')
+        self.emotion_list = ['气愤', '担心', '厌恶', '相信', '开心', '惊喜', '期待', '伤心', '无']
+        self.radio_list = ["1", "-1", "0"]
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("标注工具")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        self.menufile = QtWidgets.QMenu(self.menubar)
+        self.menufile.setObjectName("menufile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionfileopen = QtWidgets.QAction(MainWindow)
+        self.actionfileopen.setObjectName("actionfileopen")
+        self.menufile.addAction(self.actionfileopen)
+        self.menubar.addAction(self.menufile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        # 初始化主界面
+        self.initMain()
+
+    def initMain(self):
+        font = QtGui.QFont()
+        font.setFamily("Arial")  # 括号里可以设置成自己想要的其它字体
+        font.setPointSize(14)  # 括号里的数字可以设置成自己想要的字体大小
+
+        self.main_layout = QVBoxLayout()
+        self.main_widget = QWidget()
+        self.main_widget.setLayout(self.main_layout)
+
+        self.top_layout = QHBoxLayout()
+        self.top_widget = QWidget()
+        self.top_layout.addWidget(self.top_widget)
+
+        # self.pre_btn = QPushButton("上一条")
+        self.curr_label = QLabel("")
+
+
+        self.curr_label.setFont(font)
+        self.next_btn = QPushButton("下一条")
+        self.next_btn.setFont(font)
+        # self.top_layout.addWidget(self.pre_btn, 1, QtCore.Qt.AlignLeft)
+        self.top_layout.addWidget(self.curr_label, 4, QtCore.Qt.AlignLeft)
+        self.top_layout.addWidget(self.next_btn, 1, QtCore.Qt.AlignRight)
+
+
+        self.body_widget = QWidget()  # 创建窗口主部件
+        self.body_layout = QHBoxLayout()  # 创建主部件的布局
+        self.body_layout.addWidget(self.body_widget)
+
+        self.main_layout.addLayout(self.top_layout, stretch=1)
+        self.main_layout.addLayout(self.body_layout, stretch=7)
+        self.setCentralWidget(self.main_widget)  # 设置窗口主部件
+
+        self.left_widget = QWidget()  # 创建左侧部件
+        self.left_widget.setObjectName('left_widget')
+        self.left_layout = QVBoxLayout()  # 创建左侧部件的网格布局
+        self.right_widget = QWidget()  # 创建右侧部件
+        self.right_widget.setObjectName('right_widget')
+        self.right_layout = QVBoxLayout()  # 创建右侧部件的网格布局
+
+        self.left_layout.addWidget(self.left_widget)
+        self.right_layout.addWidget(self.right_widget)
+
+        self.body_layout.addLayout(self.left_layout, stretch=5)
+        self.body_layout.addLayout(self.right_layout, stretch=2)
+
+        self.content_label = QLabel("原文内容：")
+        self.content_text = QTextEdit()  # 创建文本框用于显示
+        self.content_text.setReadOnly(True)
+
+        self.comment_label = QLabel("评论内容：")
+        self.comment_text = QTextEdit()  # 创建文本框用于显示
+        self.comment_text.setReadOnly(True)
+        self.left_layout.addWidget(self.content_label, stretch=1)
+        self.left_layout.addWidget(self.content_text, stretch=10)
+        self.left_layout.addWidget(self.comment_label, stretch=1)
+        self.left_layout.addWidget(self.comment_text, stretch=5)
+
+        self.label1 = QLabel("反讽：")
+        self.label2 = QLabel("比喻：")
+        self.label3 = QLabel("夸张：")
+        self.label4 = QLabel("谐音：")
+        self.label5 = QLabel("象征：")
+        self.label6 = QLabel("情感：")  # 用下拉列表
+        self.label7 = QLabel("态度：")
+        self.label8 = QLabel("其余潜台词：")
+        font.setPointSize(10)
+        self.label1.setFont(font)
+        self.label2.setFont(font)
+        self.label3.setFont(font)
+        self.label4.setFont(font)
+        self.label5.setFont(font)
+        self.label6.setFont(font)
+        self.label7.setFont(font)
+        self.label8.setFont(font)
+
+        self.radio11 = QRadioButton("有")
+        self.radio12 = QRadioButton("没有")
+        self.radio13 = QRadioButton("不确定")
+        self.radio11.setChecked(True)
+        self.bg1 = QButtonGroup(self)
+        self.bg1.addButton(self.radio11, 0)
+        self.bg1.addButton(self.radio12, 1)
+        self.bg1.addButton(self.radio13, 2)
+        self.layout11 = QHBoxLayout()
+        self.layout11.addWidget(self.label1)
+        self.layout11.addWidget(self.radio11)
+        self.layout11.addWidget(self.radio12)
+        self.layout11.addWidget(self.radio13)
+
+        self.inputtext_1 = QLineEdit()
+        self.inputtext_1.setPlaceholderText("反讽语句")
+        self.layout1 = QHBoxLayout()
+        self.layout1.addWidget(self.inputtext_1, stretch=4)
+
+        self.radio21 = QRadioButton("有")
+        self.radio22 = QRadioButton("没有")
+        self.radio23 = QRadioButton("不确定")
+        self.radio21.setChecked(True)
+        self.bg2 = QButtonGroup(self)
+        self.bg2.addButton(self.radio21, 0)
+        self.bg2.addButton(self.radio22, 1)
+        self.bg2.addButton(self.radio23, 2)
+        self.layout21 = QHBoxLayout()
+        self.layout21.addWidget(self.label2)
+        self.layout21.addWidget(self.radio21)
+        self.layout21.addWidget(self.radio22)
+        self.layout21.addWidget(self.radio23)
+
+        self.inputtext_21 = QLineEdit()
+        self.inputtext_22 = QLineEdit()
+        self.inputtext_21.setPlaceholderText("喻体")
+        self.inputtext_22.setPlaceholderText("本体")
+        self.layout2 = QHBoxLayout()
+        self.layout2.addWidget(self.inputtext_21, stretch=2)
+        self.layout2.addWidget(self.inputtext_22, stretch=2)
+
+        self.radio31 = QRadioButton("有")
+        self.radio32 = QRadioButton("没有")
+        self.radio33 = QRadioButton("不确定")
+        self.radio31.setChecked(True)
+        self.bg3 = QButtonGroup(self)
+        self.bg3.addButton(self.radio31, 0)
+        self.bg3.addButton(self.radio32, 1)
+        self.bg3.addButton(self.radio33, 2)
+        self.layout31 = QHBoxLayout()
+        self.layout31.addWidget(self.label3)
+        self.layout31.addWidget(self.radio31)
+        self.layout31.addWidget(self.radio32)
+        self.layout31.addWidget(self.radio33)
+        self.inputtext_3 = QLineEdit()
+        self.inputtext_3.setPlaceholderText("夸张语句")
+        self.layout3 = QHBoxLayout()
+        self.layout3.addWidget(self.inputtext_3, stretch=4)
+
+        self.radio41 = QRadioButton("有")
+        self.radio42 = QRadioButton("没有")
+        self.radio43 = QRadioButton("不确定")
+        self.radio41.setChecked(True)
+        self.bg4 = QButtonGroup(self)
+        self.bg4.addButton(self.radio41, 0)
+        self.bg4.addButton(self.radio42, 1)
+        self.bg4.addButton(self.radio43, 2)
+        self.layout41 = QHBoxLayout()
+        self.layout41.addWidget(self.label4)
+        self.layout41.addWidget(self.radio41)
+        self.layout41.addWidget(self.radio42)
+        self.layout41.addWidget(self.radio43)
+        self.inputtext_41 = QLineEdit()
+        self.inputtext_42 = QLineEdit()
+        self.inputtext_41.setPlaceholderText("谐音内容")
+        self.inputtext_42.setPlaceholderText("正确内容")
+        self.layout4 = QHBoxLayout()
+        self.layout4.addWidget(self.inputtext_41, stretch=2)
+        self.layout4.addWidget(self.inputtext_42, stretch=2)
+
+        self.radio51 = QRadioButton("有")
+        self.radio52 = QRadioButton("没有")
+        self.radio53 = QRadioButton("不确定")
+        self.radio51.setChecked(True)
+        self.bg5 = QButtonGroup(self)
+        self.bg5.addButton(self.radio51, 0)
+        self.bg5.addButton(self.radio52, 1)
+        self.bg5.addButton(self.radio53, 2)
+        self.layout51 = QHBoxLayout()
+        self.layout51.addWidget(self.label5)
+        self.layout51.addWidget(self.radio51)
+        self.layout51.addWidget(self.radio52)
+        self.layout51.addWidget(self.radio53)
+        self.inputtext_51 = QLineEdit()
+        self.inputtext_52 = QLineEdit()
+        self.inputtext_51.setPlaceholderText("象征内容")
+        self.inputtext_52.setPlaceholderText("真正内容")
+        self.layout5 = QHBoxLayout()
+        self.layout5.addWidget(self.inputtext_51, stretch=2)
+        self.layout5.addWidget(self.inputtext_52, stretch=2)
+
+        # 实例化QComBox对象
+        self.cb_6 = QComboBox(self)
+        self.cb_6.addItems(self.emotion_list)
+        self.layout6 = QHBoxLayout()
+        self.layout6.addWidget(self.cb_6, stretch=1)
+
+        self.radio71 = QRadioButton("积极")
+        self.radio72 = QRadioButton("消极")
+        self.radio73 = QRadioButton("不确定")
+        self.radio71.setChecked(True)
+        self.bg7 = QButtonGroup(self)
+        self.bg7.addButton(self.radio71, 0)
+        self.bg7.addButton(self.radio72, 1)
+        self.bg7.addButton(self.radio73, 2)
+        self.input_7 = QLineEdit()
+        self.input_7.setPlaceholderText("态度信息")
+
+        self.layout7 = QHBoxLayout()
+        self.layout7.addWidget(self.radio71)
+        self.layout7.addWidget(self.radio72)
+        self.layout7.addWidget(self.radio73)
+
+        self.input_8 = QLineEdit()
+        self.input_8.setPlaceholderText("其他潜台词信息")
+        self.layout8 = QVBoxLayout()
+        self.save_btn = QPushButton("保存")
+        self.save_btn.setFont(font)
+        self.layout8.addWidget(self.input_8, stretch=1)
+        self.layout8.addWidget(self.save_btn, stretch=1)
+
+        self.right_layout.addLayout(self.layout11)
+        self.right_layout.addLayout(self.layout1)
+        self.right_layout.addLayout(self.layout21)
+        self.right_layout.addLayout(self.layout2)
+        self.right_layout.addLayout(self.layout31)
+        self.right_layout.addLayout(self.layout3)
+        self.right_layout.addLayout(self.layout41)
+        self.right_layout.addLayout(self.layout4)
+        self.right_layout.addLayout(self.layout51)
+        self.right_layout.addLayout(self.layout5)
+        self.right_layout.addWidget(self.label6)
+        self.right_layout.addLayout(self.layout6)
+        self.right_layout.addWidget(self.label7)
+        self.right_layout.addLayout(self.layout7)
+        self.right_layout.addWidget(self.label8)
+        self.right_layout.addLayout(self.layout8)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menufile.setTitle(_translate("MainWindow", "文件"))
+        self.actionfileopen.setText(_translate("MainWindow", "打开文件..."))
+
+    def _cleanText(self):
+
+        self.inputtext_1.setText("")
+        self.inputtext_21.setText("")
+        self.inputtext_22.setText("")
+        self.inputtext_3.setText("")
+        self.inputtext_41.setText("")
+        self.inputtext_42.setText("")
+        self.inputtext_51.setText("")
+        self.inputtext_52.setText("")
+        self.input_8.setText("")
+        self.cb_6.setCurrentIndex(0)
+        self.radio11.setChecked(True)
+        self.radio21.setChecked(True)
+        self.radio31.setChecked(True)
+        self.radio41.setChecked(True)
+        self.radio51.setChecked(True)
+        self.radio71.setChecked(True)
+
